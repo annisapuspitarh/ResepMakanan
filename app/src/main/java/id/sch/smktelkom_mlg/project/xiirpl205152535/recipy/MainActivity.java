a68bf2708f404d209c2f58db39da14e3ac7f78c2
annisa <ichataraha@gmail.com>
2016-11-18 14:54:27 +0700

    Action Bar Search

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl205152535/recipy/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl205152535/recipy/MainActivity.java
@@ -2,12 +2,40 @@ package id.sch.smktelkom_mlg.project.xiirpl205152535.recipy;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    }
+
