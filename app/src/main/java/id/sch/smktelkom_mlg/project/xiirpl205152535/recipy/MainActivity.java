1ff9ca4d92d3e4c5958c5d14d91bd940983673ac
annisa <ichataraha@gmail.com>
2016-11-18 13:30:01 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl205152535/recipy/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl205152535.recipy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
