3895db7bd097f801eba22b11c3aa5e38b1d30186
annisa <ichataraha@gmail.com>
2016-11-19 08:28:33 +0700

    Fragment2

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl205152535/recipy/activity/MessagesFragment.java
@@ -0,0 +1,47 @@
+package id.sch.smktelkom_mlg.project.xiirpl205152535.recipy.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.sch.smktelkom_mlg.project.xiirpl205152535.recipy.R;
+
+/**
+ * Created by Anisa_anisa on 19/11/2016.
+ */
+public class MessagesFragment extends Fragment {
+
+    public MessagesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+    }
+}
